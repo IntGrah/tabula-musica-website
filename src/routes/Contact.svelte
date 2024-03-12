fcb90c3cd0edeb1eb6f9a936d710ec425d00f5af
IntGrah <148660026+IntGrah@users.noreply.github.com>
2024-03-12 02:18:06 +0000

    s

---
+++ src/routes/Contact.svelte
@@ -0,0 +1,43 @@
+<script>
+    import InstagramLogo from "/static/social/instagram.jpg"
+</script>
+
+<div class="flex max-sm:flex-col py-6">
+	<div class="grow min-w-80">
+		<h2 class="text-lg font-serif uppercase tracking-widest text-cyan-100/80">Contact</h2>
+		<div class="py-4 font-mono text-neutral-200">
+			<p>
+				<a class="hover:underline" href="mailto:tabulamusica@cambridgesu.co.uk">
+					tabulamusica@cambridgesu.co.uk
+				</a>
+			</p>
+			<p>
+				<a class="group" href="https://www.instagram.com/tabula_musica">
+					<img class="inline" src={InstagramLogo} alt="Instagram logo" width={20} />{' '}
+					<span class="group-hover:underline"> @tabula_musica </span>
+				</a>
+			</p>
+		</div>
+	</div>
+	<div class="grow">
+		<h3 class="text-lg font-serif uppercase tracking-widest text-cyan-100/80">Subscribe</h3>
+		<div class="flex sm:max-md:flex-col gap-4 py-4 text-lg text-neutral-200">
+			<p>
+				<a
+					class="px-4 py-2 rounded-full tracking-wider font-serif text-cyan-800 transition-colors bg-amber-100 hover:bg-amber-100/90"
+					href="https://lists.srcf.net/mailman/listinfo"
+				>
+					Mailing&nbsp;List
+				</a>
+			</p>
+			<p>
+				<a
+					class="px-4 py-2 rounded-full tracking-wider font-serif text-cyan-800 transition-colors bg-amber-100 hover:bg-amber-100/90"
+					href="/#contact"
+				>
+					Magazine
+				</a>
+			</p>
+		</div>
+	</div>
+</div>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,21 @@
+<footer id="contact" className="px-8 bg-gray-900">
+    <div className="m-auto w-full max-w-5xl">
+        <Contact />
+        <hr className="border-neutral-600" />
+        <Legal />
+        </div>
+</footer>
+
+function Contact() {
+    return (
+        
+    );
+}
+
+function Legal() {
+    return (
+        <p className="py-4 text-xs font-mono tracking-wide text-neutral-400">
+            © 2024 Tabula Musica. All rights reserved.
+        </p>
+    );
+}
